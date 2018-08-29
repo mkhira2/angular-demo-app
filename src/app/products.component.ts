ee3c92fca8e38327c2dbaac2e065725e8441b3a5
Kenji Hirabayashi <mkhira2@gmail.com>
2018-08-29 05:46:58 -0500

    template syntax

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -4,4 +4,13 @@ import { Component } from "@angular/core";
     selector: 'app-products',
     templateUrl: './products.component.html'
 })
-export class ProductsComponent {}
+export class ProductsComponent {
+    productName = 'A Book';
+    isDisabled = true;
+
+    constructor() {
+        setTimeout(() => {
+            this.isDisabled = false;
+        }, 3000);
+    }
+}
