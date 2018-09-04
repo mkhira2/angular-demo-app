e8f7221925134d0940b18ea325af1580e91e9f77
Kenji Hirabayashi <mkhira2@gmail.com>
2018-09-04 04:24:38 -0500

    cross component communication

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -7,10 +7,15 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
   @Input() productName: string;
+  @Output() productClicked = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClicked() {
+    this.productClicked.emit();
   }
 
 }
