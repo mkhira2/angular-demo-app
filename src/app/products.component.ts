e46e7b616c0a98d5b528e8cf2c1e5a15c70878c1
Kenji Hirabayashi <mkhira2@gmail.com>
2018-09-11 22:43:26 -0500

    forms

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -15,8 +15,10 @@ export class ProductsComponent {
         }, 3000);
     }
 
-    onAddProduct() {
-        this.products.push(this.productName);
+    onAddProduct(form) {
+        if (form.valid) {
+            this.products.push(form.value.productName);
+        }
     }
 
     onRemoveProduct(productName: string) {
